5449ec4f5550bec275a066820d9aa9dcac2b58cd
hamed <amzhamed@gmail.com>
2023-02-22 09:57:14 -0800

    add random point cloud

--- server.py
+++ server.py
@@ -4,24 +4,27 @@ import time
 import message
 import worker
 
+import numpy as np
+
 
 if __name__ == '__main__':
     count = 10
+    random_point_cloud = np.random.randint(10, size=(10, 3))
     # Create a list of processes
     processes = []
     for i in range(count):
-        p = worker.WorkerProcess(i)
+        p = worker.WorkerProcess(i, random_point_cloud[i], random_point_cloud[i])
         p.start()
         processes.append(p)
 
-    time.sleep(2)
+    time.sleep(3)
     # Broadcast STOP message to processes
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.settimeout(0.2)
     broadcast_address = ("<broadcast>", 5000)
-    message = message.Message(message.MessageTypes.STOP, "Thank you worker")
+    message = message.Message(message.MessageTypes.STOP, -1, -1)
     sock.sendto(pickle.dumps(message), broadcast_address)
     sock.close()
     time.sleep(1)
