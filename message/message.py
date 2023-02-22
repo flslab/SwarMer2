b9d02f3b6b497f31e861a70a0615fbeac304f78f
hamed <amzhamed@gmail.com>
2023-02-22 09:55:50 -0800

    add params to message

--- message/message.py
+++ message/message.py
@@ -1,7 +1,14 @@
 class Message:
-    def __init__(self, message_type, payload):
+    def __init__(self, message_type, fid, swarm_id, dest_fid="*", dest_swarm_id="*", location=None, params=None):
         self.type = message_type
-        self.payload = payload
+        self.fid = fid
+        self.swarm_id = swarm_id
+        self.dest_fid = dest_fid
+        self.dest_swarm_id = dest_swarm_id
+        self.location = location
+        self.params = params
 
     def __repr__(self):
-        return f"Message(type={self.type.name}, payload={self.payload})"
+        return f"Message(type={self.type.name}," \
+               f"from={self.fid}:{self.swarm_id}," \
+               f"to={self.dest_fid}:{self.dest_swarm_id})"
