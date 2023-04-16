d3bc71cc9a54f26c2cfaacec23499f0a034db722
hamed <amzhamed@gmail.com>
2023-04-16 11:55:47 -0700

    add setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#git clone https://github.com/flslab/SwarMer.git
+#cd SwarMer
+#git branch feature_lease
+
 sudo apt update
 sudo apt install python3-pip
 pip3 install -r requirements.txt
-git branch feature_lease
 echo "now run python3 server.py"
