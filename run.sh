477a017f4d8f3bcca1c29a9d89e3fb1e77960096
hamed <amzhamed@gmail.com>
2023-04-22 18:55:33 -0700

    fix run

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-for i in {1..7..2}
-do
-   cp "./experiments/config$i.py" config.py
-   sleep 1
-   python server.py
-done
-
-for i in {2..8..2}
+for i in {1..8}
 do
    cp "./experiments/config$i.py" config.py
    sleep 1
