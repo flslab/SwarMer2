f6465c61a491a886ff62fc1ffbd1fe87a501f5b1
hamed <amzhamed@gmail.com>
2023-08-04 14:27:25 -0700

    experiment aws

--- start_cluster_aws.sh
+++ start_cluster_aws.sh
@@ -6,7 +6,7 @@ now=$(date +%d_%b_%H_%M_%S)
 port="6000"
 for (( i=1; i<N; i++ )); do
     server_addr=${USERNAME}@${HOSTNAMES[$i]}
-    ssh -o ConnectionAttempts=60 -oStrictHostKeyChecking=no -i ${LOCAL_KEY_PATH} "${server_addr}" "ulimit -n 9999 && cd SwarMer && cp experiments/test_config$1.py test_config.py && sleep 10 && nohup python3 server.py ${N} ${i} ${now} ${port} > my.log 2>&1 &" &
+    ssh -o ConnectionAttempts=60 -oStrictHostKeyChecking=no -i ${LOCAL_KEY_PATH} "${server_addr}" "ulimit -n 9999 && cd SwarMer && cp experiments/config$1.py config.py && sleep 10 && nohup python3 server.py ${N} ${i} ${now} ${port} > my.log 2>&1 &" &
 done
 
-ulimit -n 9999 && cp "experiments/test_config$1.py" test_config.py && sleep 1 && python3 server.py "${N}" 0 "${now}" "${port}"
+ulimit -n 9999 && cp "experiments/config$1.py" config.py && sleep 1 && python3 server.py "${N}" 0 "${now}" "${port}"
