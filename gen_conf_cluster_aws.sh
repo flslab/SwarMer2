e470e20b4c6ed142dca746fa66713b4543f1a5fb
hamed <amzhamed@gmail.com>
2023-08-04 14:25:36 -0700

    experiment aws

--- gen_conf_cluster_aws.sh
+++ gen_conf_cluster_aws.sh
@@ -2,9 +2,9 @@
 
 source aws_vars.sh
 
-python3 gen_conf.py -t
+python3 gen_conf.py
 
 for (( i=1; i<N; i++ )); do
     server_addr=${USERNAME}@${HOSTNAMES[$i]}
-    ssh -oStrictHostKeyChecking=no -i ${LOCAL_KEY_PATH} "${server_addr}" "cd SwarMer && python3 gen_conf.py -t" &
+    ssh -oStrictHostKeyChecking=no -i ${LOCAL_KEY_PATH} "${server_addr}" "cd SwarMer && python3 gen_conf.py" &
 done
