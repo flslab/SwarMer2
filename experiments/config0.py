53eaaff8219433df35e3629acbd5cf003c0e5468
hamed <amzhamed@gmail.com>
2023-09-16 11:46:08 -0700

    experiment

--- experiments/config0.py
+++ experiments/config0.py
@@ -31,7 +31,7 @@ class Config:
     MIN_ADJUSTMENT = 0
     SAMPLE_SIZE = 0
     DURATION = 300
-    SHAPE = 'chess'
+    SHAPE = 'dragon'
     RESULTS_PATH = 'results'
     MULTICAST = True
     THAW_MIN_NUM_SWARMS = 1
