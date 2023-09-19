3f6531a70c4291e332ee32e7b34ca7592b81b0ad
hamed <amzhamed@gmail.com>
2023-09-19 11:14:23 -0700

    experiment

--- aws_vars.sh
+++ aws_vars.sh
@@ -14,20 +14,3 @@ HOSTNAMES=(
 "172.31.43.137"
 "172.31.35.139"
 )
-
-34.228.230.66	–	172.31.34.114	launch-wizard-6	hamed-vir	2023/09/19 11:05 GMT-7	Linux/UNIX
-i-006c8cd49803cf6bc
-Pending
-u-6tb1.112xlarge	–	us-east-1a	3.91.40.46	–	172.31.32.226	launch-wizard-6	hamed-vir	2023/09/19 11:05 GMT-7	Linux/UNIX
-i-079042414afbe339c
-Pending
-u-6tb1.112xlarge	Initializing	us-east-1a	18.206.182.9	–	172.31.39.17	launch-wizard-6	hamed-vir	2023/09/19 11:05 GMT-7	Linux/UNIX
-i-064e448be0370aeea
-Pending
-u-6tb1.112xlarge	Initializing	us-east-1a	3.89.229.209	–	172.31.47.191	launch-wizard-6	hamed-vir	2023/09/19 11:05 GMT-7	Linux/UNIX
-i-0486f600a88010d71
-Pending
-u-6tb1.112xlarge	–	us-east-1a	18.234.47.98	–	172.31.43.137	launch-wizard-6	hamed-vir	2023/09/19 11:05 GMT-7	Linux/UNIX
-i-01b9f260ffdd43269
-Pending
-u-6tb1.112xlarge	–	us-east-1a	3.85.52.227	–	172.31.35.139
