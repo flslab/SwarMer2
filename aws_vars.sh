dfbb1ac42276c372016c2bd203d2b69a96666167
hamed <amzhamed@gmail.com>
2023-09-19 18:29:22 -0700

    experiment

--- aws_vars.sh
+++ aws_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-N=8 # number of total servers
+N=16 # number of total servers
 USERNAME="ubuntu"
 KEY_PATH="/Users/hamed/Desktop/hamed-vir.pem"
 LOCAL_KEY_PATH="~/SwarMer/hamed-vir.pem"
@@ -15,4 +15,12 @@ HOSTNAMES=(
 "172.31.42.125"
 "172.31.39.171"
 "172.31.41.31"
+"172.31.23.82"
+"172.31.27.228"
+"172.31.31.247"
+"172.31.23.216"
+"172.31.30.248"
+"172.31.19.43"
+"172.31.29.140"
+"172.31.17.31"
 )
