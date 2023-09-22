1e0c1bdc055b9e86f7605ab531ba86ce687d7162
hamed <amzhamed@gmail.com>
2023-09-22 13:06:07 -0700

    experiment aws

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ class Constants:
     WORKER_ADDRESS = ("", 5000)
 
     # SERVER_ADDRESS = ("10.0.1.1", 6000)  # cloudlab
-    SERVER_ADDRESS = ("172.31.40.199", 6000)  # aws
+    SERVER_ADDRESS = ("172.31.34.174", 6000)  # aws
     MULTICAST_GROUP_ADDRESS = ('224.3.29.25', 5000)
     MULTICAST_GROUP = '224.3.29.25'
 
