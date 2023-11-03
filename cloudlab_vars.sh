55be908c9e7150085ce310268e404de5a81a3968
hamed <amzhamed@gmail.com>
2023-11-03 14:46:02 -0700

    update vars

--- cloudlab_vars.sh
+++ cloudlab_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 idx=1
-num_of_total_servers=16
-HOSTNAME="swarmer.nova-PG0.clemson.cloudlab.us"
+num_of_total_servers=10
+HOSTNAME="swarmer.nova-PG0.wisc.cloudlab.us"
 USERNAME="hamedamz"
