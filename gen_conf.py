5269e6c16b4c90eec39c402ee3dda09553faf464
hamed <amzhamed@gmail.com>
2024-03-31 21:17:07 -0700

    exp

--- gen_conf.py
+++ gen_conf.py
@@ -61,7 +61,7 @@ props = [
     {
         "keys": ["SHAPE", "GROUP_TYPE"],
         "values": [
-            {"SHAPE": "'chess_408_spanning_3'", "GROUP_TYPE": "'spanning_2'"},
+            {"SHAPE": "'chess_100_spanning_3'", "GROUP_TYPE": "'spanning_2'"},
             # {"SHAPE": "'grid_144_spanning_2'", "GROUP_TYPE": "'spanning_2'"},
             # {"SHAPE": "'grid_225_spanning_2'", "GROUP_TYPE": "'spanning_2'"},
             # {"SHAPE": "'grid_324_spanning_2'", "GROUP_TYPE": "'spanning_2'"},
