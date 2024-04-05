a560a6c6de41a482856899e3d60ac22d2867bce3
hamed <amzhamed@gmail.com>
2024-04-05 14:32:08 -0700

    exp aws

--- gen_conf.py
+++ gen_conf.py
@@ -36,7 +36,7 @@ def_conf = {
     "SAMPLE_SIZE": "0",
     "DURATION": "60",
     "SHAPE": "'outring'",
-    "RESULTS_PATH": "'results_all'",
+    "RESULTS_PATH": "'results_v3'",
     "MULTICAST": "True",
     "THAW_MIN_NUM_SWARMS": "1",
     "THAW_PERCENTAGE_LARGEST_SWARM": "80",
