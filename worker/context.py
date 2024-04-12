7d508f451fc629349dd2552b55ac566e7da6a3f7
hamed <amzhamed@gmail.com>
2024-04-12 07:10:53 -0700

    exp cl

--- worker/context.py
+++ worker/context.py
@@ -191,7 +191,8 @@ class WorkerContext:
                     for i in range(len(path) - 1):
                         if path[i] != path[i + 1]:
                             self.absolute_poses[fid] += self.tree[path[i]][path[i+1]]['p']
-                            self.rd[fid] = max(self.rd[fid], np.linalg.norm(self.tree[path[i]][path[i+1]]['p']))
+                            self.rd[fid] += np.linalg.norm(self.tree[path[i]][path[i+1]]['p'])
+                    self.rd[fid] /= len(path)
 
     def update_relative_pose(self):
         if self.fid == 13:
